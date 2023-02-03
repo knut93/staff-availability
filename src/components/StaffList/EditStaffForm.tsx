356a9a64826cc8130b3d8ef0f98b85a49af5372a
knut93 <kgtto96@gmail.com>
2023-02-02 18:38:35 -0800

    Finished EditStaffForm

--- src/components/StaffList/EditStaffForm.tsx
+++ src/components/StaffList/EditStaffForm.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import useFile from '../../hooks/use-file'
+import { StaffObject } from '../../context/file';
 import { Staff } from './StaffList';
 
 interface AvailabilityInterface {
@@ -13,27 +14,60 @@ interface AvailabilityInterface {
 }
 function EditForm(props: { currentEditStaff: Staff, setCurrentEditStaff: Function }) {
     const { currentEditStaff, setCurrentEditStaff } = props;
-    const [monAvail, setMonAvail] = useState<string | undefined>();
-    const [tueAvail, setTueAvail] = useState<string | undefined>();
-    const [wedAvail, setWedAvail] = useState<string | undefined>();
-    const [thuAvail, setThuAvail] = useState<string | undefined>();
-    const [friAvail, setFriAvail] = useState<string | undefined>();
-    const [satAvail, setSatAvail] = useState<string | undefined>();
-    const [sunAvail, setSunAvail] = useState<string | undefined>();
+    const { currentFile, setCurrentFile } = useFile();
+    const [newAvail, setNewAvail] = useState<AvailabilityInterface>({
+        mon: "",
+        tue: "",
+        wed: "",
+        thu: "",
+        fri: "",
+        sat: "",
+        sun: ""
+    });
 
     useEffect(() => {
-        setMonAvail(currentEditStaff.mon);
-        setTueAvail(currentEditStaff.tue);
-        setWedAvail(currentEditStaff.wed);
-        setThuAvail(currentEditStaff.thu);
-        setFriAvail(currentEditStaff.fri);
-        setSatAvail(currentEditStaff.sat);
-        setSunAvail(currentEditStaff.sun);
-    }, []);
+        if (currentEditStaff.mon) {
+            setNewAvail(prevState => ({ ...prevState, mon: currentEditStaff.mon }))
+        }
+        if (currentEditStaff.tue) {
+            setNewAvail(prevState => ({ ...prevState, tue: currentEditStaff.tue }))
+        }
+        if (currentEditStaff.wed) {
+            setNewAvail(prevState => ({ ...prevState, wed: currentEditStaff.wed }))
+        }
+        if (currentEditStaff.thu) {
+            setNewAvail(prevState => ({ ...prevState, thu: currentEditStaff.thu }))
+        }
+        if (currentEditStaff.fri) {
+            setNewAvail(prevState => ({ ...prevState, fri: currentEditStaff.fri }))
+        }
+        if (currentEditStaff.sat) {
+            setNewAvail(prevState => ({ ...prevState, sat: currentEditStaff.sat }))
+        }
+        if (currentEditStaff.sun) {
+            setNewAvail(prevState => ({ ...prevState, sun: currentEditStaff.sun }))
+        }
+    }, [currentFile]);
 
+    
     const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+        let editedUser = {
+            ...newAvail,
+            firstName: currentEditStaff.firstName
+        }
+        let tempStaff = currentFile.staff.filter(a => a.firstName !== currentEditStaff.firstName);
+        tempStaff.push(editedUser)
+        setCurrentFile((previous: StaffObject) => {
+            return {
+                ...previous,
+                staff: tempStaff
+                    
+            }
+        });
     }
+
+    
 
     return (
         <div className="shadow-sm border-2 border-black-600 p-3 rounded text-2x1 max-w-[50%]">
@@ -43,31 +77,31 @@ function EditForm(props: { currentEditStaff: Staff, setCurrentEditStaff: Functio
                 </div>
                 <div className="table-row space-y-3">
                     <label className="table-cell">Mon: </label>
-                    <input value={monAvail} onChange={e => setMonAvail(e.target.value)} id="mon" type="block text" className='table-cell border-2 rounded border-gray-600-p-1' />
+                    <input value={newAvail.mon} onChange={e => setNewAvail(prevState => ({ ...prevState, mon: e.target.value }))} id="mon" type="block text" className='table-cell border-2 rounded border-gray-600-p-1' />
                 </div>
                 <div className="table-row space-y-3">
                     <label className="table-cell">Tue: </label>
-                    <input value={tueAvail} onChange={e => setTueAvail(e.target.value)} id="tue" type="block text" className='table-cell border-2 rounded border-gray-600-p-1' />
+                    <input value={newAvail.tue} onChange={e => setNewAvail(prevState => ({ ...prevState, tue: e.target.value }))} id="tue" type="block text" className='table-cell border-2 rounded border-gray-600-p-1' />
                 </div>
                 <div className="table-row space-y-3">
                     <label className="table-cell">Wed: </label>
-                    <input value={wedAvail} onChange={e => setWedAvail(e.target.value)} id="wed" type="block text" className='table-cell border-2 rounded border-gray-600-p-1' />
+                    <input value={newAvail.wed} onChange={e => setNewAvail(prevState => ({ ...prevState, wed: e.target.value }))} id="wed" type="block text" className='table-cell border-2 rounded border-gray-600-p-1' />
                 </div>
                 <div className="table-row space-y-3">
                     <label className="table-cell">Thu: </label>
-                    <input value={thuAvail} onChange={e => setThuAvail(e.target.value)} id="thu" type="block text" className='table-cell border-2 rounded border-gray-600-p-1' />
+                    <input value={newAvail.thu} onChange={e => setNewAvail(prevState => ({ ...prevState, thu: e.target.value }))} id="thu" type="block text" className='table-cell border-2 rounded border-gray-600-p-1' />
                 </div>
                 <div className="table-row space-y-3">
                     <label className="table-cell">Fri: </label>
-                    <input value={friAvail} onChange={e => setFriAvail(e.target.value)} id="fri" type="block text" className='table-cell border-2 rounded border-gray-600-p-1' />
+                    <input value={newAvail.fri} onChange={e => setNewAvail(prevState => ({ ...prevState, fri: e.target.value }))} id="fri" type="block text" className='table-cell border-2 rounded border-gray-600-p-1' />
                 </div>
                 <div className="table-row space-y-3">
                     <label className="table-cell">Sat: </label>
-                    <input value={satAvail} onChange={e => setSatAvail(e.target.value)} id="sat" type="block text" className='table-cell border-2 rounded border-gray-600-p-1' />
+                    <input value={newAvail.sat} onChange={e => setNewAvail(prevState => ({ ...prevState, sat: e.target.value }))} id="sat" type="block text" className='table-cell border-2 rounded border-gray-600-p-1' />
                 </div>
                 <div className="table-row space-y-3">
                     <label className="table-cell">Sun: </label>
-                    <input value={sunAvail} onChange={e => setSunAvail(e.target.value)} id="sun" type="block text" className='table-cell border-2 rounded border-gray-600-p-1' />
+                    <input value={newAvail.sun} onChange={e => setNewAvail(prevState => ({ ...prevState, sun: e.target.value }))} id="sun" type="block text" className='table-cell border-2 rounded border-gray-600-p-1' />
                 </div>
                 <button type="submit" className="bg-blue-100">Confirm</button>
             </form>
@@ -78,6 +112,7 @@ function EditStaffForm() {
     const { currentFile, setCurrentFile } = useFile();
     const [editForm, setEditForm] = useState(false);
     const [currentEditStaff, setCurrentEditStaff] = useState<Staff>({ firstName: "" });
+    
 
     const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
         e.preventDefault();
